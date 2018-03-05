91ccede3cf07063ea5894f0362f3b0440ebafab9
bauer-alex <alex.bauer93@gmx.net>
2018-03-05 16:18:26 +0100

    AStA paper update

--- 201803_Paper_AStA/figures_codes/seat_distribution.R
+++ 201803_Paper_AStA/figures_codes/seat_distribution.R
@@ -5,9 +5,27 @@ library(dplyr)
 library(tidyr)
 library(purrr)
 library(ggplot2)
+library(ggridges)
+library(png)
+library(grid)
 theme_set(theme_bw())
 
 # Data and code source: https://gist.github.com/adibender/28041453a2a8c3e42c09484a55668d55
+
+
+# Prepare KOALA logo for branding all plots -------------------------------
+img <- readPNG("../figures_codes/Koala_Logo_Schrift.png", info = FALSE)
+grob_img <- grobTree(rasterGrob(img, vjust = 0))
+
+# Helper function to brand a ggplot-based plot
+# NOTE: This worked fine as one didn't apply the rev_date y axis in the
+#       ridgeline plot!
+brand_plot <- function(gg, xmin = -Inf, ymin = -Inf, xmax = Inf, ymax = Inf) {
+  gg + annotation_custom(grob_img, xmax = xmax, ymax = ymax, xmin = xmin, ymin = ymin)
+}
+
+
+
 
 # Create data -------------------------------------------------------------
 ### Scrape data from before the German federal election 2013
@@ -39,7 +57,33 @@ set.seed(2018)
 shares_list <- lapply(surveys$date, function(dat) sim_oneDate(dat))
 shares <- bind_rows(shares_list)
 
-# Helper function for plot over time --------------------------------------
+
+
+# 1) Plot for the last poll before election day ---------------------------
+shares_short <- shares %>% filter(date == max(date))
+
+gg_shares <- ggplot(shares_short,
+                    aes(x = percent, y = date, group = date, # basic aesthetics
+                        fill = ifelse(..x..>50, "yes", "no"), # "cut-off" gradient
+                        frame = date, cumulative = TRUE)) + # aesthetics for animation
+  geom_density_ridges_gradient(
+    scale=10, size = 0.25, rel_min_height = 0.03, calc_ecdf=TRUE) +
+  scale_fill_manual(values = c("grey80","steelblue"), na.value = "grey80", guide = guide_legend(title = "Seat majority")) +
+  geom_vline(xintercept = 50, lty = 1, lwd = 1.2, col = "grey90") +
+  scale_x_continuous(labels = function(x) paste0(x, "%"), limits = c(40,57)) +
+  xlab("Share of parliament seats") + ylab("Density") +
+  theme_bw(base_size = 25) +
+  theme(legend.position = "bottom",
+        axis.ticks.y = element_blank(),
+        axis.text.y = element_blank())
+
+pdf("../figures/bauer_seatDist_lastPreelectionPoll2013.pdf", width = 7, height = 4)
+gg_shares
+# brand_plot(gg_shares, xmin = 53, xmax = 57.75, ymin = shares_short$date[1] + 0.65)
+dev.off()
+
+
+# 2) Helper function for plot over time -----------------------------------
 ## define function to reverse date time scale
 # source: https://gist.github.com/adibender/28041453a2a8c3e42c09484a55668d55
 library(scales)
@@ -56,9 +100,7 @@ c_trans <- function(a, b, breaks = b$breaks, format = b$format) {
 }
 
 
-# Plot over time ----------------------------------------------------------
-library(ggridges)
-
+# 2) Plot over time -------------------------------------------------------
 # revert time axis
 rev_date <- c_trans("reverse", "time")
 
@@ -71,13 +113,13 @@ gg_shares <- ggplot(shares,
     scale=10, size = 0.25, rel_min_height = 0.03, calc_ecdf=TRUE) +
   scale_fill_manual(values = c("grey80","steelblue"), na.value = "grey80", guide = guide_legend(title = "Seat majority")) +
   geom_vline(xintercept = 50, lty = 1, lwd = 1.2, col = "grey90") +
-  scale_x_continuous(labels = function(x) paste0(x, "%")) +
+  scale_x_continuous(labels = function(x) paste0(x, "%"), limits = c(40,57)) +
   scale_y_continuous(trans  = rev_date, breaks = as.POSIXct(c("2013-01-01","2013-04-01","2013-07-01","2013-09-22")),
                      labels = c("Jan 2013","Apr 2013","Jul 2013","Election day")) +
   xlab("Share of parliament seats") + ylab("") +
   theme_bw(base_size = 25) +
   theme(legend.position = "bottom")
 
-pdf("../figures/bauer_seatDist_time.pdf", width = 7, height = 6)
+pdf("../figures/bauer_seatDist_time.pdf", width = 7.5, height = 6)
 gg_shares
 dev.off()
