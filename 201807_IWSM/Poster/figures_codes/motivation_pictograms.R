48b86ea09a513a019ea1ccc4dced448d8c95fa2c
bauer-alex <alex.bauer93@gmx.net>
2018-05-23 14:01:43 +0200

    IWSM Poster

--- 201807_IWSM/Poster/figures_codes/motivation_pictograms.R
+++ 201807_IWSM/Poster/figures_codes/motivation_pictograms.R
@@ -19,6 +19,27 @@ ggplot(dat3, aes(x = party, weight = share, fill = party)) +
         plot.margin=unit(c(0,0,0,0),"mm")) +
   ggsave("../figures/motivation_survey.pdf", height = 5, width = 8)
 
+
+# Survey chart highlighted ------------------------------------------------
+dat3b <- dat3
+dat3b$col <- as.character(dat3$col)
+dat3b$col[c(1,4)] <- "#27719C"
+dat3b$col[c(2,3,5,6,7)] <- "#d1d0ce"
+ggplot(dat3b, aes(x = party, weight = share, fill = party)) +
+  geom_bar() +
+  scale_fill_manual(values = as.character(dat3b$col)) +
+  scale_y_continuous("Share in %") +
+  theme_bw(base_size = 43) +
+  theme(legend.position = "none",
+        axis.title.x = element_blank(),
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        plot.margin=unit(c(0,0,12,0),"mm")) +
+  ggsave("../figures/motivation_survey_highlighted.pdf", height = 5, width = 8)
+
+
 # Bar chart ---------------------------------------------------------------
 dat <- data.frame("party" = LETTERS[1:5],
                   "share" = c(30,20,12,14,11))
@@ -54,3 +75,23 @@ gg2 + geom_density(fill = "gray50", col = "gray40") +
   ggsave("../figures/motivation_pictoDens.pdf", height = 4, width = 5)
 gg2 + geom_density(fill = "#33CC00", col = "#3CA518") +
   ggsave("../figures/motivation_pictoDens_col.pdf", height = 4, width = 5)
+
+
+
+# Density chart highlighted -----------------------------------------------
+dat2b <- dat2
+dens_dat <- data.frame(x = density(dat2$x)$x, y = density(dat2$x)$y)
+ggplot(dat2b, aes(x = x)) +
+  geom_density(fill = "gray50", col = NA) +
+  geom_ribbon(data = dens_dat[dens_dat$x <= 50,], aes(x = x, ymax = y), ymin = 0, fill = "#d1d0ce") +
+  geom_ribbon(data = dens_dat[dens_dat$x >= 50,], aes(x = x, ymax = y), ymin = 0, fill = "#27719C") +
+  # geom_line(data = dens_dat, aes(x = x, y = y)) +
+  theme_bw(base_size = 43) +
+  # scale_x_continuous(name = "Joint party share in %") +
+  scale_x_continuous(breaks = c(30,40,50,60), labels = c(46,48,50,52)) +
+  scale_y_continuous(name = "Density") +
+  theme(axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        axis.title.x = element_blank(),
+        plot.margin=unit(c(0,0,0,0),"mm")) +
+  ggsave("../figures/motivation_density_highlighted.pdf", height = 5, width = 8)
