58ff88dde89e9a9d3f1368c0f04d47edfda3a0e7
bauer-alex <alex.bauer93@gmx.net>
2018-10-15 19:24:16 +0200

    Daten für Graefe-Bachelorarbeit

---
+++ 201810_Graefe_BA/Berechnung.R
@@ -0,0 +1,31 @@
+
+library(dplyr); library(tidyr); library(magrittr)
+
+n_votes <- 1000
+nsim <- 10000
+
+stimmen <- data.frame("A" = seq(50, 46, by = -0.5),
+                      "E" = seq(3,  7,  by = 0.5),
+                      "B" = 22,
+                      "C" = 13,
+                      "D" = 12,
+                      "pollster" = "mockup",
+                      "date" = seq.Date(as.Date("2018-10-15"), as.Date("2018-10-24"), length.out = 9),
+                      "start" = "2018-10-15",
+                      "end" = "2018-10-15",
+                      "respondents" = n_votes)
+
+stimmen %<>% gather("party", "percent", -pollster, -date, -start, -end, -respondents) %>%
+  mutate(votes = n_votes * percent / 100) %>%
+  nest(-pollster, -date, -start, -end, -respondents, .key = "survey")
+
+for (i in 1:nrow(stimmen)) {
+  set.seed(2018)
+  message(paste0("Wahrscheinlichkeit für Szenario ",i,":"))
+  prob <- stimmen %>% slice(i) %>%
+    coalitions::get_probabilities(nsim = nsim, coalitions = list(c("A","E"),c("A"))) %>%
+    unnest() %>%
+    filter(coalition == "A_E") %>%
+    pull(probability)
+  message(paste0(prob, "%"))
+}
---
+++ 201810_Graefe_BA/_Readme.txt
@@ -0,0 +1,12 @@
+Dieser Ordner enthält die von Andreas Graefe für eine Bachelorarbeit anhand der
+unteren Mail angefragten Koalitionswahrscheinlichkeiten für ausgewählte Szenarien.
+
+
+Mail von Andreas vom 13.10.18:
+
+Ich habe eine Bitte. Eine Studentin von mir schreibt Ihre Bachelorarbeit zur Wahrnehmung von Koalitionswahrscheinlichkeiten. Wir möchten untersuchen, inwieweit Wähler eigentlich die Auswirkungen der Unterschiede in den Umfrageergebnissen kleiner Parteien (um die 5% Hürde) auf die Koalitionswahrscheinlichkeiten einschätzen können. Insgesamt planen wir 9 Gruppen, wobei jeweils die Stimmenanteile der Parteien A und E angepasst werden. Sonstige Parteien werden der Einfachheit halber mit 0% angenommen (siehe Anhang).
+
+Könntest Du mir für diese 9 Szenarien jeweils die von Euch berechnete Koalitionswahrscheinlichkeit für eine Koalition der Parteien A+E zukommen lassen?
+
+Viele Grüße,
+Andreas
