d3228ceffbe4a13cbbf7e3688cb30dd2fc3f551a
bauer-alex <alex.bauer93@gmx.net>
2018-10-17 17:13:44 +0200

    Graefe BA Daten

--- 201810_Graefe_BA/Berechnung.R
+++ 201810_Graefe_BA/Berechnung.R
@@ -4,6 +4,36 @@ library(dplyr); library(tidyr); library(magrittr)
 n_votes <- 1000
 nsim <- 10000
 
+
+# Version 1 ---------------------------------------------------------------
+stimmen <- data.frame("A" = seq(50, 46, by = -0.5),
+                      "E" = seq(3,  7,  by = 0.5),
+                      "B" = 22,
+                      "C" = 13,
+                      "D" = 12,
+                      "pollster" = "mockup",
+                      "date" = seq.Date(as.Date("2018-10-15"), as.Date("2018-10-24"), length.out = 9),
+                      "start" = "2018-10-15",
+                      "end" = "2018-10-15",
+                      "respondents" = n_votes)
+
+stimmen %<>% gather("party", "percent", -pollster, -date, -start, -end, -respondents) %>%
+  mutate(votes = n_votes * percent / 100) %>%
+  nest(-pollster, -date, -start, -end, -respondents, .key = "survey")
+
+for (i in 1:nrow(stimmen)) {
+  set.seed(2018)
+  message(paste0("Wahrscheinlichkeit für Szenario ",i,":"))
+  prob <- stimmen %>% slice(i) %>%
+    coalitions::get_probabilities(nsim = nsim, coalitions = list(c("A","E"),c("A"))) %>%
+    unnest() %>%
+    filter(coalition == "A_E") %>%
+    pull(probability)
+  message(paste0(prob, "%"))
+}
+
+
+# Version 2 ---------------------------------------------------------------
 stimmen <- data.frame("A" = seq(46, 42, by = -0.5),
                       "E" = seq(3,  7,  by = 0.5),
                       "B" = 22,
@@ -30,3 +60,34 @@ for (i in 1:nrow(stimmen)) {
     pull(probability)
   message(paste0(prob, "%"))
 }
+
+
+
+# Version 3 ---------------------------------------------------------------
+stimmen <- data.frame("A" = seq(46, 42, by = -0.5),
+                      "E" = seq(3,  7,  by = 0.5),
+                      "B" = 22,
+                      "C" = 11,
+                      "D" = 10,
+                      "others" = 8,
+                      "pollster" = "mockup",
+                      "date" = seq.Date(as.Date("2018-10-15"), as.Date("2018-10-24"), length.out = 9),
+                      "start" = "2018-10-15",
+                      "end" = "2018-10-15",
+                      "respondents" = n_votes)
+
+stimmen %<>% gather("party", "percent", -pollster, -date, -start, -end, -respondents) %>%
+  mutate(votes = n_votes * percent / 100) %>%
+  nest(-pollster, -date, -start, -end, -respondents, .key = "survey")
+
+for (i in 1:nrow(stimmen)) {
+  set.seed(2018)
+  message(paste0("Wahrscheinlichkeit für Szenario ",i,":"))
+  prob <- stimmen %>% slice(i) %>%
+    coalitions::get_probabilities(nsim = nsim, coalitions = list(c("A","E"),c("A"))) %>%
+    unnest() %>%
+    filter(coalition == "A_E") %>%
+    pull(probability)
+  message(paste0(prob, "%"))
+}
+
